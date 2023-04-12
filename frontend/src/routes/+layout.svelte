c8d17a6b59daa1ad9ef6088752c205957651b4cd
Kwakyejin <ng4812@naver.com>
2023-04-12 15:38:38 +0900

    #init

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+  import "../app.postcss";
+  import { page } from "$app/stores";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
+  import "./styles.css";
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img src="/Frame.svg" class="self-center w-4/6 py-3" alt="Logo" />
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true} class="flex items-center text-xl">
+      <img src="/EMMA.svg" class="w-9 h-10 px-3" alt="Logo" />MY EMMA</NavLi
+    >
+    <NavLi href="/about" class="flex items-center  text-xl">
+      <img src="/EMMA.svg" class="w-9 h-10 px-3" alt="Logo" />ABOUT</NavLi
+    >
+  </NavUl>
+</Navbar>
+
+<div class="px-2 sm:px-4">
+  <div class="mx-auto flex mb-20 flex-col container">
+    <slot />
+  </div>
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="bg-cover bg-indigo-200">
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</div>
