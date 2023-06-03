ad5d2394ab4c48cee76011b98fd4989f50a02684
Kwakyejin <ng4812@naver.com>
2023-06-03 19:26:41 +0900

    ##backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,8 @@ from db.models import Base, Person
 from db.database import engine
 from sqlalchemy import update
 import json
+import ast
+
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
@@ -12,7 +14,7 @@ app = FastAPI()
 async def get_Person(id: str):
     with db_context() as s:
         t = s.query(Person).filter(Person.id == id).first()
-    return t.Interest[1]
+    return t
 
 @app.get("/upload")
 async def run_upload(input_a: str, input_b: str):
@@ -21,23 +23,44 @@ async def run_upload(input_a: str, input_b: str):
         s.add(t)
         s.commit()
         s.refresh(t)
-    return t.id
+    return t
 
 @app.get("/upload_interest")
 async def run_upload_interest(id: str, input_a: str, input_b: str):
     with db_context() as s:
         t =  s.query(Person).filter(Person.id == id).first()
         if t.Interest is None:
-            # t.Interest = [input_a, input_b]
-            t.Interest = json.dumps([input_a, input_b],  ensure_ascii=False)
+            #t.Interest = []
+            #t.Interest = [input_a, input_b]
+            t.Interest = json.dumps(input_a, input_b,  ensure_ascii=False)
         else:
             #t.Interest += [input_a, input_b]
-            t.Interest += json.dumps([input_a, input_b],  ensure_ascii=False)
-            setattr(t, 'Interest', t.Interest)
+            t.Interest += json.dumps(input_a, input_b,  ensure_ascii=False)
+            #setattr(t, 'Interest', t.Interest)
         s.commit()
         s.refresh(t)
+        t.Interest = json.loads(t.Interest)
         #I = json.load(t.Interest)
         #I.append([input_a, input_b]))
+        #x = ast.literal_eval(str(t.Interest))
+    return t.Interest
+
+@app.post("/upload_interest")
+async def run_upload_interest(id: str, input_a: str, input_b: str):
+    with db_context() as s:
+        t =  s.query(Person).filter(Person.id == id).first()
+        if t.Interest is None:
+            t.Interest = json.dumps([input_a, input_b], ensure_ascii=False)
+        else:
+            try:
+                interests = json.loads(t.Interest)
+            except json.JSONDecodeError:
+                interests = []
+            interests.append([input_a, input_b])
+            t.Interest = json.dumps(interests, ensure_ascii=False)
+        s.commit()
+        s.refresh(t)
+        t.Interest = json.loads(t.Interest)
     return t.Interest
 
 @app.get("/upload_qual")
